506bcb624134bf9942c7e2e6d4824a46e642103c
Jeff Smith + Kyle Snell <apprentice@dbc25.local>
2012-11-27 11:50:54 -0800

    added javascript facebook login; need to fix popup functionality

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,6 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
+  helper_method :current_user
 
 private
 
@@ -11,6 +12,7 @@ private
     @current_user ||= User.find(session[:user_id]) if session[:user_id]
   end
   
+
   helper_method :current_user
 
 end
