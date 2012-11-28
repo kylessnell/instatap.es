5196f2c564b44dd3daa10080dde5fc6cefbdcb3b
Jeff Smith + Kyle Snell <apprentice@dbc25.local>
2012-11-28 11:13:28 -0800

    updated facebook ENV variables for Heroku

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,5 +1,5 @@
 OmniAuth.config.logger = Rails.logger
 
 Rails.application.config.middleware.use OmniAuth::Builder do
-  provider :facebook, FACEBOOK_KEY, FACEBOOK_SECRET  # In production, make sure to add ENV[] to key and secret
+  provider :facebook, ENV['FACEBOOK_KEY'], ENV['FACEBOOK_SECRET']  # In production, make sure to add ENV[] to key and secret
 end
