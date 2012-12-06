61291af2e806a99bda8c1b16d8325ce74886a674
Apprentice <apprentice@dbc25.local>
2012-12-05 17:04:10 -0800

    authorization (mixtape/new and design are restricted). javascript for playing songs on any page. puts are replaced by Rails.logger

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,4 +1,8 @@
 class StaticPagesController < ApplicationController
+  include SessionsHelper
+  helper_method :current_user
+  skip_filter :authorize_user
+   
   def home
     @mixtape = Mixtape.last
   end
