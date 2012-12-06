2ba4912bb894bb80880aa5d5d156f587aa48960a
jeffpersonified <jeff.matthew.smith@gmail.com>
2012-12-06 12:21:43 -0800

    add arrows to buttons

--- app/controllers/mixtapes_mailers_controller.rb
+++ app/controllers/mixtapes_mailers_controller.rb
@@ -22,13 +22,4 @@ class MixtapesMailersController < ApplicationController
     })
     redirect_to mixtape_play_path(@mixtape.url)
   end
-  # def create
-  #   @recipient_name = params[:recipient_name]
-  #   @author_name = params[:author_name]
-  #   @recipient_email = params[:recipient_email]
-  #   @mixtape = Mixtape.find_by_url(params[:url])
-  #   MixtapesMailer.mixtape_delivery(@recipient_email, @author_name).deliver
-  #   redirect_to mixtape_play_path(@mixtape.url)
-  # end
-
 end
