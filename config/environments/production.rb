3435c94b59c9199cda39eca74a249a0521075891
Guillaume Galuz <guillaume.galuz@gmail.com>
2012-12-06 12:18:42 -0800

    commented a charset line in production.rb

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -25,7 +25,7 @@ Mixtapes::Application.configure do
   # Generate digests for assets URLs
   config.assets.digest = true
   config.action_mailer.delivery_method = :smtp
-  config.action_mailer.default_charset = "utf-8"
+  # config.action_mailer.default_charset = "utf-8"
   config.action_mailer.perform_deliveries = true
   config.action_mailer.raise_delivery_errors = true
 
