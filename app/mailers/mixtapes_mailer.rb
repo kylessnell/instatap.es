031b6c78a276036c2f257a3bd7a1a414acd05af4
Kyle Snell <kylessnell@gmail.com>
2012-12-06 20:16:45 -0800

    updated mailer to sendgrid

--- app/mailers/mixtapes_mailer.rb
+++ app/mailers/mixtapes_mailer.rb
@@ -1,11 +1,8 @@
 class MixtapesMailer < ActionMailer::Base
-  default from: "mixtapes.yo@gmail.com"
+    default :from => "instatap.es@gmail.com"
 
-  def mixtape_delivery(recipient_email, author_name)
-    @recipient_name = params[:recipient_name]
-    @author_name = params[:author_name]
-    @recipient_email = params[:recipient_email]
-    @mixtape = Mixtape.find_by_url(params[:url])
-    mail(:to => recipient_email, :subject => "#{author_name} made you an Instatape!")
+    def mixtape_delivery(author_name, recipient_email)
+      mail( :to => recipient_email,
+      :subject => "#{author.name} has shared an Instatape with you!" )
+    end
   end
-end
